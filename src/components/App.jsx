259626a71a75cc6d6062b84e84c1bf6ae201c528
dayana <dayana.romero88@gmail.com>
2024-03-27 19:50:25 +0100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react"
+import HookUseCallback from "./HookUseCallback";
+
+
+function App() {
+  const inputRef = useRef(null);
+  const [count, setCount] = useState(0)
+  const previewCount = useRef()
+
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
+
+  useEffect(() => {
+    focusInput()
+  }, [])
+
+  const handleClick = () => {
+    previewCount.current = count;
+    setCount(count + 1)
+  }
+  return (
+    <>
+
+      <div>
+        <h1>Al cargar la pagina aparezca el cursor en el input </h1>
+        <input type="text" ref={inputRef} />
+        <br></br>
+        <button onClick={handleClick}>Incremente el contador</button>
+        <h3>El valor anterior es {previewCount.current !== undefined ? previewCount.current : "N/A"}</h3>
+        <h3>El valor actual es: {count}</h3>
+      </div>
+
+      <HookUseCallback />
+
+    </>
+  )
+}
+
+export default App
